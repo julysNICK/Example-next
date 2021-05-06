0a056cb15789a1c57d17c4958f6755cdae0c8bbb
Julys Martins <julystube@gmail.com>
2021-05-06 20:00:08 -0300

    adicionando example next

---
+++ src/components/CloneMe/stories.tsx
@@ -0,0 +1,16 @@
+
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { CloneMe, CloneMeProps } from '.';
+
+export default {
+  title: 'CloneMe',
+  component: CloneMe,
+} as Meta;
+
+export const Template: Story<CloneMeProps> = (args) => {
+  return (
+    <div>
+      <CloneMe {...args} />
+    </div>
+  );
+};
